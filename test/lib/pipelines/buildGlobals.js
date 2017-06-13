bc1e7e5dbe0b982d3ef4ebdf1249d49fec6ddb49
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-06-13 16:25:10 -0700

    Combine test files now that there is only one pipeline file

--- test/lib/pipelines/buildGlobals.js
+++ test/lib/pipelines/buildGlobals.js
@@ -1,17 +1,22 @@
 'use strict';
 
 var assert = require('assert');
+var babelDeps = require('babel-deps');
 var buildGlobals = require('../../../lib/pipelines/buildGlobals');
 var consume = require('stream-consume');
 var vfs = require('vinyl-fs');
 
+var defaultOptions = {
+	sourceMaps: false
+};
+
 describe('Pipeline - Build to globals', function() {
 	it('should build js files to a globals bundle and its source map', function(done) {
 		var stream = vfs.src('test/fixtures/js/foo.js')
-      .pipe(buildGlobals());
+			.pipe(buildGlobals());
 
 		var files = [];
-    stream.on('data', function(file) {
+		stream.on('data', function(file) {
 			files.push(file.relative);
 		});
 		stream.on('end', function() {
@@ -24,12 +29,12 @@ describe('Pipeline - Build to globals', function() {
 
 	it('should build js files to a globals bundle without its source map', function(done) {
 		var stream = vfs.src('test/fixtures/js/foo.js')
-      .pipe(buildGlobals({
+			.pipe(buildGlobals({
 				sourceMaps: false
 			}));
 
 		var files = [];
-    stream.on('data', function(file) {
+		stream.on('data', function(file) {
 			files.push(file.relative);
 		});
 		stream.on('end', function() {
@@ -38,5 +43,65 @@ describe('Pipeline - Build to globals', function() {
 			done();
 		});
 		consume(stream);
+	});
+});
+
+describe('Pipeline - Build to globals with no source maps', function() {
+	beforeEach(function() {
+		babelDeps.clearCache();
+	});
+
+	it('should build js files to a single bundle', function(done) {
+		var stream = vfs.src('test/fixtures/js/foo.js')
+			.pipe(buildGlobals(defaultOptions));
+
+		var files = [];
+		stream.on('data', function(file) {
+			files.push(file.relative);
+		});
+		stream.on('end', function() {
+			assert.strictEqual(1, files.length);
+			assert.strictEqual('metal.js', files[0]);
+			done();
+		});
+		consume(stream);
+	});
+
+	it('should build js files to single bundle with the specified filename', function(done) {
+		var stream = vfs.src('test/fixtures/js/foo.js')
+			.pipe(buildGlobals({
+				bundleFileName: 'foo.js',
+				sourceMaps: false
+			}));
+
+		stream.on('data', function(file) {
+			assert.strictEqual('foo.js', file.relative);
+			done();
+		});
+	});
+
+	it('should publish exported variables on global', function(done) {
+		var stream = vfs.src('test/fixtures/js/foo.js')
+			.pipe(buildGlobals(defaultOptions));
+
+		stream.on('data', function(file) {
+			var contents = file.contents.toString();
+			assert.notStrictEqual(-1, contents.indexOf('this["metal"] ='));
+			done();
+		});
+	});
+
+	it('should publish exported variables on specified global', function(done) {
+		var stream = vfs.src('test/fixtures/js/foo.js')
+			.pipe(buildGlobals({
+				globalName: 'bar',
+				sourceMaps: false
+			}));
+
+		stream.on('data', function(file) {
+			var contents = file.contents.toString();
+			assert.notStrictEqual(-1, contents.indexOf('this["bar"] ='));
+			done();
+		});
 	});
 });
